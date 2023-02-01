9c9eeef520c3d392c4d1205cacab0bfb2cf408c0
Skull <12714893+SkullDesperados@users.noreply.github.com>
2023-02-01 16:49:17 +0100

    Update install-ruby-openssl-codeploy.sh

--- install-ruby-openssl-codeploy.sh
+++ install-ruby-openssl-codeploy.sh
@@ -2,14 +2,19 @@
 
 ## Install RVM packages managers for ruby
 gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable --ruby
+dnf install curl gcc bzip2 openssl-devel libffi-devel readline-devel zlib-devel ncurses-devel which
+curl -sSL https://get.rvm.io | bash -s stable
+bash
 
 ## Install OpenSSL 1.1 and Ruby 2.7 with RVM
 rvm pkg install openssl
 rvm install ruby-2.7 --with-openssl-dir=/usr/local/rvm/usr
 
 ## Install AWS CodeDeploy Agent
-yum install chkconfig
+dnf install chkconfig wget
+mkdir /opt/aws-codedeploy 
+cd /opt/aws-codedeploy
 wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
+service codedeploy-agent status
